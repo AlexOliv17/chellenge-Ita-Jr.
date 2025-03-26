ec4abb2371b82cf664b078eb4f68aaef2f43ad4b
Alex Oliveira <alex.oliveira@b2ml.com.br>
2025-03-26 20:00:44 -0300

    init project

---
+++ chellenge/src/main/java/com/itauJr/chellenge/ChellengeApplication.java
@@ -0,0 +1,13 @@
+package com.itauJr.chellenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChellengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChellengeApplication.class, args);
+	}
+
+}
---
+++ chellenge/src/test/java/com/itauJr/chellenge/ChellengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itauJr.chellenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChellengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
